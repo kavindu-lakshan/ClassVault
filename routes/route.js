f07b7902d7e1ed30b993b14e904e3dc0e5d122ad
LahiruPrasad12 <lahirupr471@gmail.com>
2023-03-22 18:19:59 +0530

    implemented register part

--- routes/route.js
+++ routes/route.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { createStackNavigator } from "@react-navigation/stack";
-import AdminDashboard from '../screens/admin/Dashboard'
+import AdminDashboard from '../layouts/Side-Nav'
 import Login from '../screens/auth/login'
 
 const Stack = createStackNavigator();
