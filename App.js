1d085485dc7c37dd35916eaa6406639d10f91499
LahiruPrasad12 <lahirupr471@gmail.com>
2023-03-22 16:21:32 +0530

    dashboard cards css done

--- App.js
+++ App.js
@@ -2,10 +2,11 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import SideNav from './layouts/Side-Nav'
 import Users from './screens/admin/Users/Users'
+import Dashboard from "./screens/admin/Dashboard";
 export default function App() {
     return (
         <NavigationContainer>
-            <Users/>
+            <SideNav/>
         </NavigationContainer>
     );
 }
