f9cb0ebcfa7182f83ad681573c84d733e25f48bc
LahiruPrasad12 <lahirupr471@gmail.com>
2023-03-22 15:35:51 +0530

    user page css done

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import SideNav from './layouts/Side-Nav'
-
+import Users from './screens/admin/Users/Users'
 export default function App() {
     return (
         <NavigationContainer>
-            <SideNav/>
+            <Users/>
         </NavigationContainer>
     );
 }
--- screens/admin/Users/Users.jsx
+++ screens/admin/Users/Users.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {ScrollView, StyleSheet, Text, View} from "react-native";
+import {ScrollView, StyleSheet, View, Text, TouchableOpacity, Image, TextInput} from "react-native";
 import {Button, Searchbar} from 'react-native-paper';
 import SelectDropdown from 'react-native-select-dropdown'
 
@@ -21,6 +21,9 @@ export default function Users() {
 
     const toggleDialog1 = () => {
         setVisible1(!visible1);
+    };
+    const toggleDialog2 = () => {
+        alert("Hehe hari bn");
     };
 
 
@@ -55,64 +58,28 @@ export default function Users() {
             </View>
 
             <ScrollView>
-                <Card containerStyle={{marginTop: 15}}>
-                    <Dialog.Title title="Dialog Title"/>
-                    <Card.Divider/>
-                    <Text h1>
-                        h1 Heading
-                    </Text>
-                    <Text h2>
-                        h2 Heading
-                    </Text>
-                    <Text h3>
-                        h3 Heading
-                    </Text>
-                    <Text h4>
-                        h4 Heading
-                    </Text>
-                    <Text>Normal Text</Text>
-                </Card>
+                <View style={styles.userDetailsCard}>
+                    <Image source={require('../../../assets/download.jpg')} style={styles.avatar} />
+                    <View style={styles.detailsContainer}>
+                        <Text style={styles.name}>Name : Ishara</Text>
+                        <Text style={styles.email}>Email: Ishara@gmail.com</Text>
+                    </View>
+                    <TouchableOpacity style={styles.viewMoreButton}>
+                        <Text style={styles.viewMoreButtonText}>View More</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.userDetailsCard}>
+                    <Image source={require('../../../assets/download.jpg')} style={styles.avatar} />
+                    <View style={styles.detailsContainer}>
+                        <Text style={styles.name}>Name : Ishara</Text>
+                        <Text style={styles.email}>Email: Ishara@gmail.com</Text>
+                    </View>
+                    <TouchableOpacity style={styles.viewMoreButton} onPress={toggleDialog1} >
+                        <Text style={styles.viewMoreButtonText}>View More</Text>
+                    </TouchableOpacity>
+                </View>
 
-                <Card containerStyle={{marginTop: 15}}>
-                    <Dialog.Title title="Dialog Title"/>
-                    <Button icon="camera" onPress={toggleDialog1}>
-                        Press me
-                    </Button>
-                    <Card.Divider/>
-                    <Text h1>
-                        h1 Heading
-                    </Text>
-                    <Text h2>
-                        h2 Heading
-                    </Text>
-                    <Text h3>
-                        h3 Heading
-                    </Text>
-                    <Text h4>
-                        h4 Heading
-                    </Text>
-                    <Text>Normal Text</Text>
-                </Card>
-                <Card containerStyle={{marginTop: 15}}>
-                    <Dialog.Title title="Dialog Title"/>
-                    <Button icon="camera" onPress={toggleDialog1}>
-                        Press me
-                    </Button>
-                    <Card.Divider/>
-                    <Text h1>
-                        h1 Heading
-                    </Text>
-                    <Text h2>
-                        h2 Heading
-                    </Text>
-                    <Text h3>
-                        h3 Heading
-                    </Text>
-                    <Text h4>
-                        h4 Heading
-                    </Text>
-                    <Text>Normal Text</Text>
-                </Card>
+
             </ScrollView>
 
 
@@ -121,27 +88,37 @@ export default function Users() {
                 onBackdropPress={toggleDialog1}
             >
 
-                <Card containerStyle={{ marginTop: 15 }}>
-
-                    <Button icon="camera" onPress={toggleDialog1}>
-                        Press me
-                    </Button>
-
-                    <Card.Divider />
-                    <Text h1>
-                        h1 Heading
-                    </Text>
-                    <Text h2>
-                        h2 Heading
-                    </Text>
-                    <Text h3>
-                        h3 Heading
-                    </Text>
-                    <Text h4>
-                        h4 Heading
-                    </Text>
-                    <Text>Normal Text</Text>
-                </Card>
+                <View>
+                    <Image source={require('../../../assets/download.jpg')} style={styles.avatarShow} />
+                    <Text style={styles.detailsTag}>Name</Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholderTextColor="#aaaaaa"
+                        underlineColorAndroid="transparent"
+                        autoCorrect={false}
+                        placeholder="Ishara Madusanka"
+                        autoCapitalize="none"
+                        secureTextEntry={true}
+                    />
+                   <Text style={styles.detailsTag}>Email</Text>
+                    <TextInput
+                    style={styles.input}
+                    placeholderTextColor="#aaaaaa"
+                    underlineColorAndroid="transparent"
+                    autoCorrect={false}
+                    placeholder="Ishara@gmail.com"
+                    autoCapitalize="none"
+                    secureTextEntry={true}
+                />
+                    <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 16 ,marginTop:20}}>
+                    <TouchableOpacity style={styles.editButton} onPress={toggleDialog2} >
+                        <Text style={styles.dialogButtonText}>Edit</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.deleteButton} onPress={toggleDialog2} >
+                        <Text style={styles.dialogButtonText} >Delete</Text>
+                    </TouchableOpacity>
+                    </View>
+                </View>
             </Dialog>
 
         </View>
@@ -149,14 +126,88 @@ export default function Users() {
 }
 
 const styles = StyleSheet.create({
-    container: {
+    userDetailsCard: {
+        backgroundColor: '#f7f7f7',
+        borderColor: '#ccc',
+        borderWidth: 1,
+        borderRadius: 5,
+        padding: 20,
+        marginTop: 20,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.3,
+        shadowRadius: 5,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    detailsContainer: {
         flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
+        marginLeft: 10,
     },
-    view: {
-        margin: 10,
+    avatar: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
     },
+    name: {
+        fontWeight: 'bold',
+    },
+    email: {
+        color: '#666',
+    },
+    viewMoreButton: {
+        backgroundColor: '#ccc',
+        padding: 10,
+        borderRadius: 5,
+    },
+    viewMoreButtonText: {
+        color: '#fff',
+        fontWeight: 'bold',
+    },
+    input: {
+        height: 48,
+        borderRadius: 5,
+        overflow: "hidden",
+        backgroundColor: "white",
+        paddingLeft: 16,
+        flex: 1,
+        marginRight: 5,
+        marginTop:20
+    },
+    detailsTag:{
+        marginTop:20,
+        fontWeight: 'bold',
+    },
+    avatarShow:{
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        marginLeft: 'auto',
+        marginRight: 'auto'
+    },
+    editButton :{
+        backgroundColor: 'blue',
+        paddingVertical: 12,
+        paddingHorizontal: 16,
+        paddingTop:0,
+        borderRadius: 8
+    },
+    dialogButtonText:{
+        color: 'white',
+        fontWeight: 'bold'
+    },
+    deleteButton:{
+        backgroundColor: 'red',
+        paddingVertical: 16,
+        paddingHorizontal: 16,
+        paddingTop:0,
+        borderRadius: 8
+    }
+
+
 });
 
