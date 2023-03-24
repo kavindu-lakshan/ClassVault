d1b19dd022dc4ca6a7ed1d0fc377b33e74ebe6d5
Kavindu Lakshan <75882836+kavindu-lakshan@users.noreply.github.com>
2023-03-25 03:05:20 +0530

    Add animations to checker dashboard

--- screens/checker/checker.js
+++ screens/checker/checker.js
@@ -1,14 +1,87 @@
-import {
-  View,
-  Text,
-  FlatList,
-  StyleSheet,
-  TouchableOpacity,
-  Dimensions,
-} from "react-native";
-
+import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
+import React from "react";
+import LottieView from "lottie-react-native";
 import { useNavigation } from "@react-navigation/native";
 
-const Checker = () => {};
+const MyButton = ({ onPress, title }) => {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      style={{
+        backgroundColor: "#2196F3",
+        borderRadius: 40,
+        width: 200,
+        height: 75,
+        justifyContent: "center",
+      }}
+    >
+      <Text
+        style={{
+          fontSize: 24,
+          color: "white",
+          textAlign: "center",
+          fontWeight: 500,
+        }}
+      >
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+const Checker = () => {
+  const navigation = useNavigation();
+  return (
+    <View>
+      <View style={styles.lottieCOntainer}>
+        <LottieView
+          source={require("../../assets/teach.json")}
+          autoPlay
+          loop
+          resizeMode="contain"
+        />
+      </View>
+      <View>
+        <Text
+          style={{
+            fontSize: 32,
+            fontWeight: "bold",
+            textAlign: "center",
+            paddingTop: 20,
+          }}
+        >
+          Welcome...!
+        </Text>
+      </View>
+      <View style={{ justifyContent: "center", alignItems: "center" }}>
+        <Text
+          style={{
+            fontSize: 24,
+            fontWeight: "400",
+            textAlign: "center",
+            paddingTop: 20,
+            paddingBottom: 30,
+            paddingLeft: 10,
+            paddingRight: 10,
+          }}
+        >
+          Please click the button below to navigate to the Notices section.
+        </Text>
+        <MyButton
+          title="Notices"
+          onPress={() => navigation.navigate("Notices")}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  lottieCOntainer: {
+    width: 250,
+    height: 400,
+    alignSelf: "center",
+  },
+});
 
 export default Checker;
