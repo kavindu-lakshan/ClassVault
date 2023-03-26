ab984230f1cb8d6df121d73a5621ae4b35d36fa2
Kavindu Lakshan <75882836+kavindu-lakshan@users.noreply.github.com>
2023-03-26 14:55:56 +0530

    resolve conflict

--- routes/route.js
+++ routes/route.js
@@ -2,12 +2,8 @@ import * as React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 //Checker Routes
 import AdminDashboard from "../layouts/Side-Nav";
-<<<<<<< HEAD
 import Notices from "../screens/admin/Users/Users";
 import Course from "../screens/teacher/course";
-=======
-import Users from "../screens/admin/Users/Users";
->>>>>>> a9923d0d762f3a9b46ee88e4b23f136ac3f4a3ed
 
 // import AdminDashboard from "../layouts/Checker-Side-Nav";
 import TeacherDashboard from "../layouts/Teacher-Side-Nav";
@@ -18,8 +14,6 @@ import Course from "../screens/teacher/course";
 import courseUpdate from "../screens/teacher/course";
 
 const Stack = createStackNavigator();
-
-
 
 export default function Routes() {
   return (
